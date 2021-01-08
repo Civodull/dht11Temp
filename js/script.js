37a987304ae85ddc207787a0a8d9207f6bbc9526
Civodull <74184848+Civodull@users.noreply.github.com>
2021-01-08 10:49:35 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,68 +1,3 @@
-function buttonOn() {
-    if (confirm('CONFIRMEZ-VOUS D ACTIVER LA VANE ?')) {
-        // window.open('page.html', '', '');
-        document.getElementById('etat').src = 'im5';
-    }
-}
-
-function buttonOff() {
-    if (confirm('CONFIRMEZ-VOUS DE DESACTIVER LA VANE ?')) {
-        document.getElementById('etat').src = 'im4';
-    }
-}
-
-function dButtonOn() {
-    if (confirm('CONFIRMEZ-VOUS D ACTIVER LA VANE ?')) {
-        // window.open('page.html', '', '');
-        document.getElementById('etat1').src = 'im5';
-    }
-
-}
-
-function dButtonOff() {
-    if (confirm('CONFIRMEZ-VOUS DE DESACTIVER LA VANE ?')) {
-        document.getElementById('etat1').src = 'im4';
-    }
-}
-
-function tButtonOn() {
-    if (confirm('CONFIRMEZ-VOUS D ACTIVER LA VANE ?')) {
-        // window.open('page.html', '', '');
-        document.getElementById('etat2').src = 'im5';
-    }
-
-}
-
-function tButtonOff() {
-    if (confirm('CONFIRMEZ-VOUS DE DESACTIVER LA VANE ?')) {
-        document.getElementById('etat2').src = 'im4';
-    }
-}
-
-function qButtonOn() {
-    if (confirm('CONFIRMEZ-VOUS D ACTIVER LA VANE ?')) {
-        // window.open('page.html', '', '');
-        document.getElementById('etat3').src = 'im5';
-    }
-}
-
-function qButtonOff() {
-    if (confirm('CONFIRMEZ-VOUS DE DESACTIVER LA VANE ?')) {
-        document.getElementById('etat3').src = 'im4';
-    }
-}
-//creation d'une fonction d'allumage et d'eteignage deuxieme methode
-
-function etatButton(valeurRecu) {
-    var etatImage;
-    if (valeurRecu == 1) {
-        etatImage = "im4";
-    } else
-        etatImage = "im5";
-    //etatImage ne peut pas afficher l'image sur la pasge html pour le faire 
-    //il faut rajouter la balise img et recuperer son id
-    document.getElementById('img').src = etatImage;
-}
 var socket = io();
 socket.on('temp', function(data) {
     console.log(data);
@@ -70,15 +5,23 @@ socket.on('temp', function(data) {
     var teperature = `${data}`;
     //fonction pour changer d'images en fonction de la temperature
     //document.getElementById('img').src = 'im1';
-    if (data <= 26) {
-        document.getElementById('img').setAttribute('src', 'im1');
+ if (data <= 26) {
+        document.getElementById('img').innerHTML = '<img src="/im1" alt="Ambient" title="Temperature < = 26 " style="width:40px; height:44px;">';
     } else {
-        document.getElementById('img').setAttribute('src', 'imLogo');
+        document.getElementById('img').innerHTML = '<img src="/imLogo" alt="Chaud" title="Temperature >= 26 " style="width:40px; height:44px; border-radius: 32px;">';
     }
     if (data <= 26) {
         document.getElementById('rectangle2').innerHTML = '<img src="/im" alt="Il fait beau" style="width:640px; height:544px; float:left;">';
     } else {
         document.getElementById('rectangle2').innerHTML = '<img src="/im2" alt="Il fait chaud" style="width:640px; height:544px; float:left;">';
+    }
+    date = new Date();
+    mois = date.getMonth();
+    if (mois == (mois + 1) || mois == (mois + 2) || mois == (mois + 6) || mois == (mois + 8) || mois == (mois + 12)) {
+        document.getElementById('saisonn').innerHTML = "E T E";
+    }
+    if (mois == (mois + 3) || mois == (mois + 4) || mois == (mois + 5) || mois == (mois + 7) || mois == (mois + 9) || mois == (mois + 10) || mois == (mois + 11)) {
+        document.getElementById('saisonn').innerHTML = "H I V E R";
     }
 });
 
